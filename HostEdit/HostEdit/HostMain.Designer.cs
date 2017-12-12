3fe5860ce5b616702a0a07648443355f0aa071c9
Xujun Ren <v-xujren@microsoft.com>
2017-12-12 17:43:22 +0800

    add proxy ip setting

--- HostEdit/HostEdit/HostMain.Designer.cs
+++ HostEdit/HostEdit/HostMain.Designer.cs
@@ -31,16 +31,22 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HostMain));
             this.btnZhiruo = new System.Windows.Forms.Button();
             this.btnDodox = new System.Windows.Forms.Button();
+            this.tbNewIp = new System.Windows.Forms.TextBox();
+            this.lbNewIp = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.cbCurrentIpList = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnZhiruo
             // 
             this.btnZhiruo.BackColor = System.Drawing.SystemColors.Control;
-            this.btnZhiruo.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnZhiruo.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnZhiruo.ForeColor = System.Drawing.Color.DarkRed;
-            this.btnZhiruo.Location = new System.Drawing.Point(57, 101);
+            this.btnZhiruo.Location = new System.Drawing.Point(83, 237);
             this.btnZhiruo.Name = "btnZhiruo";
-            this.btnZhiruo.Size = new System.Drawing.Size(125, 33);
+            this.btnZhiruo.Size = new System.Drawing.Size(125, 36);
             this.btnZhiruo.TabIndex = 0;
             this.btnZhiruo.Text = "Host到芷若";
             this.btnZhiruo.UseVisualStyleBackColor = false;
@@ -48,22 +54,91 @@
             // 
             // btnDodox
             // 
-            this.btnDodox.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnDodox.Font = new System.Drawing.Font("Microsoft YaHei", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnDodox.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.btnDodox.Location = new System.Drawing.Point(57, 164);
+            this.btnDodox.Location = new System.Drawing.Point(258, 237);
             this.btnDodox.Name = "btnDodox";
-            this.btnDodox.Size = new System.Drawing.Size(125, 32);
+            this.btnDodox.Size = new System.Drawing.Size(125, 35);
             this.btnDodox.TabIndex = 1;
             this.btnDodox.Text = "Host到DoDox";
             this.btnDodox.UseVisualStyleBackColor = true;
             this.btnDodox.Click += new System.EventHandler(this.btnDodox_Click);
             // 
+            // tbNewIp
+            // 
+            this.tbNewIp.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNewIp.Location = new System.Drawing.Point(163, 88);
+            this.tbNewIp.Name = "tbNewIp";
+            this.tbNewIp.Size = new System.Drawing.Size(154, 29);
+            this.tbNewIp.TabIndex = 2;
+            // 
+            // lbNewIp
+            // 
+            this.lbNewIp.AutoSize = true;
+            this.lbNewIp.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNewIp.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbNewIp.Location = new System.Drawing.Point(49, 88);
+            this.lbNewIp.Name = "lbNewIp";
+            this.lbNewIp.Size = new System.Drawing.Size(92, 21);
+            this.lbNewIp.TabIndex = 3;
+            this.lbNewIp.Text = "要的加入iP:";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft YaHei", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnRemove.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnRemove.Location = new System.Drawing.Point(344, 147);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(125, 35);
+            this.btnRemove.TabIndex = 8;
+            this.btnRemove.Text = "移除当前的iP";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // cbCurrentIpList
+            // 
+            this.cbCurrentIpList.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCurrentIpList.FormattingEnabled = true;
+            this.cbCurrentIpList.Location = new System.Drawing.Point(163, 147);
+            this.cbCurrentIpList.Name = "cbCurrentIpList";
+            this.cbCurrentIpList.Size = new System.Drawing.Size(154, 29);
+            this.cbCurrentIpList.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(33, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 21);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "当前选择的iP:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft YaHei", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnAdd.Location = new System.Drawing.Point(344, 85);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(125, 35);
+            this.btnAdd.TabIndex = 15;
+            this.btnAdd.Text = "增加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
             // HostMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::HostEdit.Properties.Resources.zhiruo;
-            this.ClientSize = new System.Drawing.Size(259, 309);
+            this.ClientSize = new System.Drawing.Size(516, 368);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbCurrentIpList);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.lbNewIp);
+            this.Controls.Add(this.tbNewIp);
             this.Controls.Add(this.btnDodox);
             this.Controls.Add(this.btnZhiruo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -71,7 +146,9 @@
             this.MaximizeBox = false;
             this.Name = "HostMain";
             this.Text = "Host Update";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HostMain_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,6 +156,12 @@
 
         private System.Windows.Forms.Button btnZhiruo;
         private System.Windows.Forms.Button btnDodox;
+        private System.Windows.Forms.TextBox tbNewIp;
+        private System.Windows.Forms.Label lbNewIp;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ComboBox cbCurrentIpList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
--- HostEdit/HostEdit/HostMain.cs
+++ HostEdit/HostEdit/HostMain.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace HostEdit
@@ -8,42 +13,60 @@ namespace HostEdit
     public partial class HostMain : Form
     {
         private string hostPath;
-        private string localOriginPath;
-        private string localZhiRuoPath;
+        private string localZhiRuoIpListPath;
         public HostMain()
         {
             InitializeComponent();
+            this.tbNewIp.Text = "40.125.172.125";
             hostPath = Environment.SystemDirectory + "\\drivers\\etc\\hosts";
-            var originDirectory = Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\origin");
             var zhiRuoDirectory = Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\zhiruo");
-            localOriginPath = originDirectory.FullName + "\\hosts";
-            localZhiRuoPath = zhiRuoDirectory.FullName + "\\hosts";
-            if (!File.Exists(localOriginPath))
+            localZhiRuoIpListPath = zhiRuoDirectory.FullName + "\\ipList.bat";
+            if (File.Exists(localZhiRuoIpListPath))
             {
-                File.Copy(hostPath, localOriginPath);
-            }
-            if (!File.Exists(localZhiRuoPath))
-            {
-                File.Copy(hostPath, localZhiRuoPath);
-                File.AppendAllLines(localZhiRuoPath, new string[] { "40.125.172.125 heroii.dodox.com.cn" });
-            }
+                Dictionary<string, bool> ipListDict = new Dictionary<string, bool>();
+                FileStream fileStream;
+                using (fileStream = new FileStream(localZhiRuoIpListPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    BinaryFormatter bF = new BinaryFormatter();
+                    ipListDict = bF.Deserialize(fileStream) as Dictionary<string, bool>;
+                    foreach (var item in ipListDict)
+                    {
+                        cbCurrentIpList.Items.Add(item.Key);
+                        if (item.Value)
+                        {
+                            cbCurrentIpList.SelectedItem = item.Key;
+                        }
+                    }
+                }    
+            }      
         }
 
         private void btnZhiruo_Click(object sender, EventArgs e)
         {
-            HostAndTest(localZhiRuoPath);
+            restoveDodox();
+            File.AppendAllLines(hostPath, new string[] { $"{cbCurrentIpList.SelectedItem.ToString()} heroii.dodox.com.cn" });
+            HostAndTest();
         }
 
         private void btnDodox_Click(object sender, EventArgs e)
         {
-            File.AppendAllLines(hostPath, new string[] { "40.125.172.125 heroii.dodox.com.cn" });
-            HostAndTest(localOriginPath);          
+            restoveDodox();
+            HostAndTest();
         }
 
-        private void HostAndTest(string path)
+        private void restoveDodox()
         {
-
-            File.Copy(path, hostPath, true);
+            List<string> lines = new List<string>();
+            lines.AddRange(File.ReadAllLines(hostPath));
+            foreach (var item in cbCurrentIpList.Items)
+            {
+                lines.Remove($"{item.ToString()} heroii.dodox.com.cn");
+            }
+        
+            File.WriteAllLines(hostPath, lines.ToArray());
+        }
+        private void HostAndTest()
+        {
             CloseALLieProcess();
             ProcessStartInfo pInfo = new ProcessStartInfo("iexplore", "heroii.dodox.com.cn");
             Process.Start(pInfo);
@@ -65,6 +88,63 @@ namespace HostEdit
                     }
                 }
             }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string ipPattern = @"^(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)$";
+
+            if (!Regex.IsMatch(tbNewIp.Text, ipPattern))
+            {
+                MessageBox.Show("请输入正确的IP地址");
+                return;
+            }
+            if (!this.cbCurrentIpList.Items.Contains(tbNewIp.Text))
+            {
+                this.cbCurrentIpList.Items.Add(tbNewIp.Text);
+                if (this.cbCurrentIpList.Items.Count == 1)
+                {
+                    this.cbCurrentIpList.SelectedItem = tbNewIp.Text;
+                    this.cbCurrentIpList.Refresh();
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (this.cbCurrentIpList.SelectedItem != null)
+            {
+                object willRemoveItem = cbCurrentIpList.SelectedItem;
+                this.cbCurrentIpList.SelectedIndex = -1;
+                this.cbCurrentIpList.Items.Remove(willRemoveItem);
+                if (this.cbCurrentIpList.Items.Count!=0)
+                {
+                    this.cbCurrentIpList.SelectedItem = this.cbCurrentIpList.Items[0];
+                }            
+                this.cbCurrentIpList.Refresh();
+            }
+        }
+
+        private void HostMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Dictionary<string, bool> ipListDict = new Dictionary<string, bool>();
+            foreach (var item in cbCurrentIpList.Items)
+            {
+                if (item == cbCurrentIpList.SelectedItem)
+                {
+                    ipListDict.Add(item.ToString(), true);
+                }
+                else
+                {
+                    ipListDict.Add(item.ToString(), false);
+                }         
+            }
+            FileStream fileStream;
+            using (fileStream = new FileStream(localZhiRuoIpListPath, FileMode.Create))
+            {
+                BinaryFormatter bF = new BinaryFormatter();
+                bF.Serialize(fileStream, ipListDict);
+            }                      
         }
     }
 }
